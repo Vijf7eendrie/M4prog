5bbfa6e32c5b703e989794c5b35fa91409967ba5
rayan <rayanaghmer072@outlook.com>
2024-05-07 12:56:27 +0200

    nieuwebestanden

---
+++ dockerM4Prog/dockerM4Prog/sql/02 data/huisdierdata.sql
@@ -0,0 +1,13 @@
+INSERT INTO `Huisdieren` (`naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES
+('kat', 'rayan', 'kat', 12, '2024-05-07'),
+('kat', 'rayan', 'kat', 12, '2024-05-07'),
+('kat', 'rayan', 'kat', 12, '2024-05-07'),
+('kat', 'rayan', 'kat', 12, '2024-05-07'),
+('kat', 'rayan', 'kat', 12, '2024-05-07'),
+('kat', 'rayan', 'kat', 12, '2024-05-07'),
+('kat', 'rayan', 'kat', 12, '2024-05-07'),
+('hond', 'rayan', 'hond', 14, '2024-05-07'),
+('aap', 'rayan', 'aap', 14, '2024-05-07'),
+('vogel', 'rayan', 'vogel', 14, '2024-05-07');
+
